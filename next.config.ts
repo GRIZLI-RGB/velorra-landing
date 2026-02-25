b5db8ae7da4702dec595b3f6cfb7981818f3c8b8
GRIZLI-RGB <ek916569@gmail.com>
2026-02-25 13:17:54 +0500

    Initial Project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  poweredByHeader: false,
+  logging: false,
 };
 
 export default nextConfig;
