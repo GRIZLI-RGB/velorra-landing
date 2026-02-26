aa1d653ed9faee234adaf3cb2bd99c80ac5c8353
GRIZLI-RGB <ek916569@gmail.com>
2026-02-26 19:30:30 +0500

    Добавлен LaunchPath (шаблон) и часть тарифов

--- components/landing/tariffs.tsx
+++ components/landing/tariffs.tsx
@@ -1,10 +1,51 @@
 import clsx from "clsx";
+import Title from "./ui/title";
+
+const SUBBLOCKS = [
+  {
+    left: "До 2 999 минут",
+    right: "19 руб. / минута",
+    isBold: false,
+  },
+  {
+    left: "До 9 999 минут",
+    right: "17 руб. / минута",
+    isBold: false,
+  },
+  {
+    left: "От 10 000 минут",
+    right: "15 руб. / минута",
+    isBold: true,
+  },
+];
 
 export default function Tariffs({ className }: { className?: string }) {
   return (
     <section className={clsx(className)}>
-      <h2 className="text-2xl font-semibold">Tariffs</h2>
-      <p className="mt-2 text-base text-zinc-600">Тарифы и условия подключения.</p>
+      <div className="default-container">
+        <Title className="max-w-[700px] mx-auto">
+          Модель оплаты состоит из подписки и минут разговоров.
+        </Title>
+        <div className="mt-10 mb-5">Тарифы</div>
+        <div className="flex flex-col gap-2.5">
+          {SUBBLOCKS.map((item) => (
+            <div
+              style={{
+                boxShadow:
+                  "3px 3px 4px 0 rgba(0, 0, 0, 0.02), 5px 5px 10px 0 rgba(0, 0, 0, 0.04)",
+              }}
+              key={item.left}
+              className={clsx(
+                "flex items-center justify-between bg-[#FDFDFD] rounded-[12px] border border-[#C1C1C1] px-6 h-[60px]",
+                item.isBold && "font-bold",
+              )}
+            >
+              <span>{item.left}</span>
+              <span>{item.right}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
